b2ddce9a236a15e15f7edf152162c0b776e27ee4
NaufalRafiPratama <73165365+NaufalRafiPratama@users.noreply.github.com>
2020-11-08 00:03:01 +0700

    Add files via upload

---
+++ PBOIF2-10119062-Latihan32-DaftarFilm/src/pboif2/pkg10119062/latihan32/daftarfilm/NowPlaying.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119062.latihan32.daftarfilm;
+
+/**
+ *
+ * @author 
+ * NAMA     : Naufal Rafi Pratama
+ * KELAS    : PBOIF2
+ * NIM      : 10119062
+ * Deskripsi Program : program ini berisi program untuk menampilkan
+ * daftar film
+ */
+class NowPlaying {
+    String filmName;
+    String filmGenre;
+    double filmRating;
+    int filmDuration;
+    
+    void nowPlaying(){
+        System.out.println("===Daftar Film Sedang Tayang===");
+        System.out.println();
+        System.out.println("Judul Film : Venom");
+        System.out.println("Genre Film : Action, Horror, Scifi");
+        System.out.println("Rating Film : 8.5");
+        System.out.println("Duration Film : 120 Menit");
+        System.out.println();
+        System.out.println("Judul Film : Small Foot");
+        System.out.println("Genre Film : Animation");
+        System.out.println("Rating Film : 9.0");
+        System.out.println("Duration Film : 96 Menit");
+        System.out.println();
+        System.out.println("Judul Film : Crazy Rich Asian");
+        System.out.println("Genre Film : Comedy");
+        System.out.println("Rating Film : 7.8");
+        System.out.println("Duration Film : 119 Menit");
+        System.out.println();
+        System.out.println("Judul Film : Asih");
+        System.out.println("Genre Film : Horror");
+        System.out.println("Rating Film : 6.0");
+        System.out.println("Duration Film : 100 Menit");
+        System.out.println();
+    }
+}
---
+++ PBOIF2-10119062-Latihan32-DaftarFilm/src/pboif2/pkg10119062/latihan32/daftarfilm/PBOIF210119062Latihan32DaftarFilm.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119062.latihan32.daftarfilm;
+
+/**
+ *
+ * @author 
+ * NAMA     : Naufal Rafi Pratama
+ * KELAS    : PBOIF2
+ * NIM      : 10119062
+ * Deskripsi Program : program ini berisi program untuk menampilkan 
+ * daftar film
+ */
+public class PBOIF210119062Latihan32DaftarFilm {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        NowPlaying f = new NowPlaying();
+        f.nowPlaying();
+    }
+    
+}
